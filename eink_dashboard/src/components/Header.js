b0b8b7ef6b4f22540f1d7b23fb5ec79fa24dcc02
Andrej Sovic <andrej.sovic@gmail.com>
2025-11-22 20:47:45 +0100

    font

--- eink_dashboard/src/components/Header.js
+++ eink_dashboard/src/components/Header.js
@@ -11,9 +11,10 @@ function Header() {
     const formatDateTime = (date) => {
         const day = date.getDate().toString().padStart(2, '0');
         const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const year = date.getFullYear();
         const hours = date.getHours().toString().padStart(2, '0');
         const minutes = date.getMinutes().toString().padStart(2, '0');
-        return `${day}.${month}. @ ${hours}:${minutes}`;
+        return `${day}/${month}/${year}, ${hours}:${minutes}`;
     };
 
     return (
